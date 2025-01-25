22ecf9efba6777c436f3e8dac1ff73e8514202bf
Марина Гладких <marinagladkih@MacBook-Pro-Marina.local>
2025-01-25 12:19:57 +0300

    add slider on main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,32 @@ $('.menu-media').click(function() {
     $(this).toggleClass('menu-media--active');
     $('.menu-open').toggleClass('menu-open--active');
 });
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        500: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        810: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1100: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: '.slider-next',
+                prevEl: '.slider-prev',
+            },
+        }
+    }
+});
